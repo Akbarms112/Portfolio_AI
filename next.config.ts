46c2def4aef99922bb3430ad4fc5b4b0a7574496
Akbarali78 <akbar.y@smilodata.com>
2026-09-02 13:01:23 +0530

    feat: setup GitHub Pages auto-deploy workflow

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   output: "export",
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  basePath: isProd ? "/Portfolio_AI" : "",
+  assetPrefix: isProd ? "/Portfolio_AI/" : "",
 };
+
 export default nextConfig;
